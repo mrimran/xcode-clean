0c01d418687a7fa3daec691b415b2d37b078ff02
Imran Zahoor <mrimranzahoor@gmail.com>
2022-06-23 15:15:59 +0500

    Adde xcode cleaner

---
+++ xcode-cleaner.sh
@@ -0,0 +1,4 @@
+rm -rf ~/Library/Developer/CoreSimulator/Devices/*
+rm -rf ~/Library/Developer/Caches/dyld/*
+# rm -rf ~/Library/Developer/XCode/DerivedData/*
+rm -rf ~/Library/Developer/XCode/Archives/*
